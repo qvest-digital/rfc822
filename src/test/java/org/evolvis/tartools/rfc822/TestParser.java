b47c40f9ddb25cd91910f9e7dfdf8cd6b96920f2
mirabilos <t.glaser@tarent.de>
2020-06-28 00:17:10 +0200

    Revert "use Lombok @Cleanup"… double whammy
    • Sonar triggers “use try-with-resources, or close it explicitly”
    
    • JaCoCo triggers an untested branch (null check) — unsure whether
      reverting bb71a500f6c3c942ce25c39e1cc71d0ae479a44c and annotating
      begin() for nōn-null could help with that…
    
    This reverts commit c277987fa33753bef1764dc470577e01211b8e6d.

--- src/test/java/org/evolvis/tartools/rfc822/TestParser.java
+++ src/test/java/org/evolvis/tartools/rfc822/TestParser.java
@@ -20,7 +20,6 @@ package org.evolvis.tartools.rfc822;
  * of said person’s immediate fault when using the work as intended.
  */
 
-import lombok.Cleanup;
 import lombok.val;
 
 import java.util.ArrayList;
@@ -62,27 +61,29 @@ isWsp(final int cur, final int next)
 String
 keyword()
 {
-	@Cleanup val beg = new Parser.Txn();
-	if (skip(TestParser::isWsp) == -1)
-		return null;
-	@Cleanup val ofs = new Parser.Txn();
-	for (final String keyword : keywords) {
-		if (s().startsWith(keyword, pos())) {
-			final int nextch = bra(keyword.length());
-			if (nextch < 'a' || nextch > 'z') {
-				final Substring s = ofs.substring();
-				// COMMIT in reverse BEGIN order
-				ofs.commit();
-				// last COMMIT implied here
-				//return beg.accept(keyword);
-				// the above would be normal but
-				// we must test the below:
-				return beg.accept(s.toString());
+	try (val beg = new Parser.Txn()) {
+		if (skip(TestParser::isWsp) == -1)
+			return null;
+		try (val ofs = new Parser.Txn()) {
+			for (final String keyword : keywords) {
+				if (s().startsWith(keyword, pos())) {
+					final int nextch = bra(keyword.length());
+					if (nextch < 'a' || nextch > 'z') {
+						final Substring s = ofs.substring();
+						// COMMIT in reverse BEGIN order
+						ofs.commit();
+						// last COMMIT implied here
+						//return beg.accept(keyword);
+						// the above would be normal but
+						// we must test the below:
+						return beg.accept(s.toString());
+					}
+					ofs.rollback();
+				}
 			}
-			ofs.rollback();
 		}
+		return null;
 	}
-	return null;
 }
 
 String
