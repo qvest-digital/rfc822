089a5f1d9fd648c5fc20e2266c676d5667f0834c
mirabilos <t.glaser@tarent.de>
2021-01-12 16:43:08 +0100

    PathTest must also be passed by UXAddress, sed it around

---
+++ src/test/java/org/evolvis/tartools/rfc822/UXAddressPathTest.sh
@@ -0,0 +1,28 @@
+#!/bin/mksh
+
+# initialisation
+export LC_ALL=C
+unset LANGUAGE
+set -e
+set -o pipefail
+
+cd "$(dirname "$0")"
+rm -f *~
+
+set +e
+x=$(sed --posix 's/u\+/x/g' <<<'fubar fuu' 2>&1) && alias 'sed=sed --posix'
+x=$(sed -e 's/u\+/x/g' -e 's/u/X/' <<<'fubar fuu' 2>&1)
+case $?:$x {
+(0:fXbar\ fuu) ;;
+(*) print -u2 'E: your sed is not POSIX compliant'; exit 255 ;;
+}
+set -e
+
+sed \
+    -e 's/Path/UXAddress/g' \
+    -e 's/UXAddressTest/UXAddressPathTest/g' \
+    -e 's/Test ..link UXAddress. class/& ({@link Path} part)/' \
+    -e 's/UXAddress\.AddressList/Path.AddressList/g' \
+    -e 's/UXAddress\.AddrSpecSIDE/Path.AddrSpecSIDE/g' \
+    <PathTest.java >UXAddressPathTest.java~
+mv UXAddressPathTest.java~ UXAddressPathTest.java
