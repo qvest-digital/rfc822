88664369a6064de7fba440517b9ddda3036db2c2
mirabilos <t.glaser@tarent.de>
2021-01-13 01:06:33 +0100

    Revert "https://github.com/rzwitserloot/lombok/issues/2502" as this works now
    This reverts commit 8726dc6d2bdde62401a04ba06804329dbd8be6d6.

--- src/main/java/org/evolvis/tartools/rfc822/Path.java
+++ src/main/java/org/evolvis/tartools/rfc822/Path.java
@@ -606,7 +606,7 @@ protected Path(final String input)
  * Parses the address as mailbox-list, e.g. for the From and Resent-From headers
  * (but see {@link #asAddressList()} for RFC6854’s RFC2026 §3.3(d) Limited Use)
  *
- * @return parser result; remember to call isValid() on it first!
+ * @return parser result; remember to call {@link Path.AddressList#isValid()} first!
  */
 public AddressList
 asMailboxList()
@@ -625,7 +625,7 @@ asMailboxList()
  *
  * @param allowRFC6854forLimitedUse use address instead of mailbox parsing
  *
- * @return parser result; remember to call isValid() on it first!
+ * @return parser result; remember to call {@link Path.Address#isValid()} first!
  */
 public Address
 forSender(final boolean allowRFC6854forLimitedUse)
@@ -660,7 +660,7 @@ asAddrSpec()
  * allows using this for the From and Resent-From headers, normally
  * covered by the {@link #asMailboxList()} method.
  *
- * @return parser result; remember to call isValid() on it first!
+ * @return parser result; remember to call {@link Path.AddressList#isValid()} first!
  */
 public AddressList
 asAddressList()
